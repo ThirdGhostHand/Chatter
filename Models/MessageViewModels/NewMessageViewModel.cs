e1bebe272f943d80b286a77325ed20ab41ce9612
ThirdGhostHand <ThirdGhostHand@outlook.com>
2017-12-12 11:44:21 -0800

    intial commit

---
+++ Models/MessageViewModels/NewMessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chatter.Models
+{
+    public class NewMessageViewModel
+    {
+        [Required]
+        public string Content { get; set; }
+    }
+}
